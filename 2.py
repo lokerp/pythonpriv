3b59ff54164d9273e01d1fc18fabc21646b0de47
lokerp <107425466+lokerp@users.noreply.github.com>
2023-04-14 12:21:16 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -4,7 +4,6 @@ from docxtpl import DocxTemplate
 
 file = None
 reader = None
-page_break_sym = "\f"
 pages = []
 
 doc_tmp = DocxTemplate("winners_doc_template.docx")
@@ -21,11 +20,26 @@ try:
 except csv.Error:
     print("Ошибка! Файл некорректного формата!")
     exit()
-reader_list = list(reader)
-reader_list.sort(key=lambda x: x.get('Year'))
+winners = []
+winners_list = list(reader)
+winners_list.sort(key=lambda x: x.get('Year'))
+for i in range(len(winners_list)):
+    year_first = winners_list[i].get('Year')
+    city_first = winners_list[i].get('City')
+    dic = {'Year': year_first,
+           'City': city_first,
+           'Winner': [{winners_list[i][k] for k in range(1, 4)}]}
+    for j in range(len(winners_list) + 1):
+        year_second = winners_list[j].get('Year')
+        city_second = winners_list[j].get('City')
+        if year_first == year_second and city_first == city_second:
+            dic.get('Winner').append({winners_list[j][k] for k in range(1, 4)})
+            winners_list.pop(j)
+    winners.append(dic)
+print(winners)
 
-print(reader_list)
-for i in range(len(reader_list)):
+print(winners_list)
+for i in range(len(winners_list)):
     page_tmp = DocxTemplate("winners_page_template.docx")
     context = {
         "year": None,
@@ -36,33 +50,33 @@ for i in range(len(reader_list)):
         "female_time": None,
         "page_break": ""
     }
-    this_year = reader_list[i].get('Year')
+    this_year = winners_list[i].get('Year')
     this_time = None
     try:
-        this_time = datetime.datetime.strptime(reader_list[i].get('Time'), '%m/%d/%y %H:%M:%S')
+        this_time = datetime.datetime.strptime(winners_list[i].get('Time'), '%m/%d/%y %H:%M:%S')
     except ValueError:
         pass
     context['year'] = this_year
-    context['city'] = reader_list[i].get('City')
-    this_sex = reader_list[i].get('Sex')
-    this_name = reader_list[i].get('Name')
+    context['city'] = winners_list[i].get('City')
+    this_sex = winners_list[i].get('Sex')
+    this_name = winners_list[i].get('Name')
     if this_sex == "Male":
         context['male_name'] = this_name
         if this_time is not None:
-            context['male_time'] = reader_list[i].get('Time')
+            context['male_time'] = winners_list[i].get('Time')
     elif this_sex == "Female":
         context['female_name'] = this_name
         if this_time is not None:
-            context['female_time'] = reader_list[i].get('Time')
+            context['female_time'] = winners_list[i].get('Time')
 
-    if i < len(reader_list) - 1:
+    if i < len(winners_list) - 1:
         next_time = None
         try:
-            next_time = datetime.datetime.strptime(reader_list[i + 1].get('Time'), '%H:%M:%S')
+            next_time = datetime.datetime.strptime(winners_list[i + 1].get('Time'), '%H:%M:%S')
         except ValueError:
             pass
-        next_sex = reader_list[i + 1].get('Sex')
-        next_name = reader_list[i + 1].get('Name')
+        next_sex = winners_list[i + 1].get('Sex')
+        next_name = winners_list[i + 1].get('Name')
         if this_time is not None and next_time is not None:
             if this_sex == next_sex and this_time <= next_time:
                 continue
